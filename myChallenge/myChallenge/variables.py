81f0e6a89396f03a63e017a34a7a53c2d3313fec
Borzase <borjazs@outlook.es>
2025-04-24 19:27:20 +0200

    progreso
    progreso

--- myChallenge/myChallenge/variables.py
+++
@@ -1,8 +0,0 @@
-"""
-variables usadas en "main.py"
-last modification: Mar 16 2025
-@authors:LV
-"""
-## se puede usar sets para las constantes
-dim_tablero  = np.full((10,10), " ") #tamano tablero
-num_barcos = 10  #numero barcos 
